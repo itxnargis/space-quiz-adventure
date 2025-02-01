7119fad102c874eb0ac51df7b9a824bca19311df
itxnargis <itxnargiskhatun@gmail.com>
2025-02-01 22:22:14 +0530

    fix - Change api frontend

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 export const fetchQuizData = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/quiz")
+      const response = await fetch("https://space-quiz-adventure.vercel.app/api/quiz")
       if (!response.ok) {
         throw new Error("Failed to fetch quiz data")
       }
